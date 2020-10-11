9166897353dd43450bebc3c93f0dc8f585351fb0
Ryan Clements <ryanclementshax@gmail.com>
2020-10-11 10:55:10 -0400

    OpenGL Shaders

--- GameEngine/src/GameEngine/Application.h
+++ GameEngine/src/GameEngine/Application.h
@@ -9,6 +9,8 @@
 
 #include "GameEngine/ImGui/ImGuiLayer.h"
 
+#include "GameEngine/Renderer/Shader.h"
+
 namespace GameEngine {
 	class GAME_ENGINE_API Application
 	{
@@ -35,6 +37,7 @@ namespace GameEngine {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
