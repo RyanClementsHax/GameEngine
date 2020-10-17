2e61ae45645b2919403a2250864e14455d9fe56f
Ryan Clements <ryanclementshax@gmail.com>
2020-10-16 20:03:01 -0400

    Shader Asset Files

--- GameEngine/src/GameEngine/Renderer/Shader.cpp
+++ GameEngine/src/GameEngine/Renderer/Shader.cpp
@@ -6,6 +6,17 @@
 
 namespace GameEngine {
 
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:	GE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:	return new OpenGLShader(filepath);
+		}
+
+		GE_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
 
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
