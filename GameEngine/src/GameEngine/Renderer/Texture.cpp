2f23c511d689ce3a234807abcf6601d28dc6edb7
Ryan Clements <ryanclementshax@gmail.com>
2020-10-19 20:23:36 -0400

    Single Shader 2D Renderer

--- GameEngine/src/GameEngine/Renderer/Texture.cpp
+++ GameEngine/src/GameEngine/Renderer/Texture.cpp
@@ -6,12 +6,24 @@
 
 namespace GameEngine {
 
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:	GE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:	return CreateRef<OpenGLTexture2D>(width, height);
+		}
+
+		GE_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
 		{
 			case RendererAPI::API::None:	GE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-			case RendererAPI::API::OpenGL:	return std::make_shared<OpenGLTexture2D>(path);
+			case RendererAPI::API::OpenGL:	return CreateRef<OpenGLTexture2D>(path);
 		}
 
 		GE_CORE_ASSERT(false, "Unknown RendererAPI!");
