9f46835094f3e32c7ed3a6a1560363209a5e0681
Ryan Clements <ryanclementshax@gmail.com>
2020-10-25 17:28:14 -0400

    Log messages are now additionally sent to GameEngine.log file

--- GameEngine/src/GameEngine/Core/Log.cpp
+++ GameEngine/src/GameEngine/Core/Log.cpp
@@ -2,6 +2,7 @@
 #include "GameEngine/Core/Log.h"
 
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace GameEngine {
 	Ref<spdlog::logger> Log::s_CoreLogger;
@@ -9,12 +10,21 @@ namespace GameEngine {
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("GameEngine.log", true));
 
-		s_CoreLogger = spdlog::stdout_color_mt("GAME_ENGINE");
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("GAME_ENGINE", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
 
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
