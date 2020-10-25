c53ffb9a0a69c009aa23cbe571f7b07b125706bd
Ryan Clements <ryanclementshax@gmail.com>
2020-10-25 16:37:33 -0400

    Auto deducing an available __FUNCSIG__ definition

--- GameEngine/src/GameEngine/Debug/Instrumentor.h
+++ GameEngine/src/GameEngine/Debug/Instrumentor.h
@@ -124,10 +124,31 @@ namespace Hazel {
 
 #define GE_PROFILE 1
 #if GE_PROFILE
+	// Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define GE_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+			#define GE_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define GE_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define GE_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define GE_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define GE_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define GE_FUNC_SIG __func__
+	#else
+		#define GE_FUNC_SIG "GE_FUNC_SIG unknown!"
+	#endif
+
 	#define GE_PROFILE_BEGIN_SESSION(name, filepath) ::Hazel::Instrumentor::Get().BeginSession(name, filepath)
 	#define GE_PROFILE_END_SESSION() ::Hazel::Instrumentor::Get().EndSession()
 	#define GE_PROFILE_SCOPE(name) ::Hazel::InstrumentationTimer timer##__LINE__(name);
-	#define GE_PROFILE_FUNCTION() GE_PROFILE_SCOPE(__FUNCSIG__)
+	#define GE_PROFILE_FUNCTION() GE_PROFILE_SCOPE(GE_FUNC_SIG)
 #else
 	#define GE_PROFILE_BEGIN_SESSION(name, filepath)
 	#define GE_PROFILE_END_SESSION()
