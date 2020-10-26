580458c88052ec8a6dce03d50d8595c245c8b486
Ryan Clements <ryanclementshax@gmail.com>
2020-10-25 20:08:49 -0400

    BATCH RENDERING

--- GameEngine/src/GameEngine/Renderer/Buffer.cpp
+++ GameEngine/src/GameEngine/Renderer/Buffer.cpp
@@ -7,6 +7,18 @@
 
 namespace GameEngine {
 
+	Ref<VertexBuffer> VertexBuffer::Create(uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:	GE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:	return CreateRef<OpenGLVertexBuffer>(size);
+		}
+
+		GE_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
