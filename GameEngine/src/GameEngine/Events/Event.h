7901b57d9c9c670b294895cbd79f14234f21d77a
Ryan Clements <ryanclementshax@gmail.com>
2020-10-25 21:22:23 -0400

    Removed redundant 'inline' keyword usage

--- GameEngine/src/GameEngine/Events/Event.h
+++ GameEngine/src/GameEngine/Events/Event.h
@@ -58,7 +58,7 @@ namespace GameEngine {
 		virtual int GetCategoryFlags() const = 0;
 		virtual std::string ToString() const { return GetName(); }
 
-		inline bool IsInCategory(EventCategory category)
+		bool IsInCategory(EventCategory category)
 		{
 			return GetCategoryFlags() & category;
 		}
