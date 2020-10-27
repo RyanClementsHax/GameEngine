2b837ac7ba1624b803137d4059015a2f51fcc6bf
Ryan Clements <ryanclementshax@gmail.com>
2020-10-26 20:44:45 -0400

    General cleanup

--- GameEngine/src/GameEngine/Core/Core.h
+++ GameEngine/src/GameEngine/Core/Core.h
@@ -44,12 +44,22 @@
 #endif // End of platform detection
 
 #ifdef GE_DEBUG
+	#if defined(GE_PLATFORM_WINDOWS)
+		#define GE_DEBUGBREAK() __debugbreak()
+	#elif defined(GE_PLATFORM_LINUX)
+		#include <signal.h>
+		#define GE_DEBUGBREAK() raise(SIGTRAP)
+	#else
+		#error "Platform doesn't support debugbreak yet!"
+	#endif
 	#define GE_ENABLE_ASSERTS
+#else
+	#define GE_DEBUGBREAK()
 #endif
 
 #ifdef GE_ENABLE_ASSERTS
-	#define GE_ASSERT(x, ...) { if(!(x)) { GE_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak(); } }
-	#define GE_CORE_ASSERT(x, ...) { if(!(x)) { GE_CORE_ERROR("Assertion failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define GE_ASSERT(x, ...) { if(!(x)) { GE_ERROR("Assertion failed: {0}", __VA_ARGS__); GE_DEBUGBREAK(); } }
+	#define GE_CORE_ASSERT(x, ...) { if(!(x)) { GE_CORE_ERROR("Assertion failed: {0}", __VA_ARGS__); GE_DEBUGBREAK(); } }
 #else
 	#define GE_ASSERT(x, ...)
 	#define GE_CORE_ASSERT(x, ...)
