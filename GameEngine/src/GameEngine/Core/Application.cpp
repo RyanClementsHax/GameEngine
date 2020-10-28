e688b389a784ea152954d3929a6c0c4b6821a6dd
Ryan Clements <ryanclementshax@gmail.com>
2020-10-27 22:17:42 -0400

    Imgui handling mouse and keyboards events

--- GameEngine/src/GameEngine/Core/Application.cpp
+++ GameEngine/src/GameEngine/Core/Application.cpp
@@ -59,9 +59,9 @@ namespace GameEngine {
 
 		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
 		{
-			(*it)->OnEvent(e);
 			if (e.Handled)
 				break;
+			(*it)->OnEvent(e);
 		}
 	}
 
