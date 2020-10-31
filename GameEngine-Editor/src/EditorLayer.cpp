8d1c71f7fd0b61b7cadddbf993145535a6dccf55
Ryan Clements <ryanclementshax@gmail.com>
2020-10-31 08:44:43 -0400

    The ENTITY class

--- GameEngine-Editor/src/EditorLayer.cpp
+++ GameEngine-Editor/src/EditorLayer.cpp
@@ -24,9 +24,9 @@ namespace GameEngine {
 
 		m_ActiveScene = CreateRef<Scene>();
 
+		// Entity
 		auto square = m_ActiveScene->CreateEntity();
-		m_ActiveScene->Reg().emplace<TransformComponent>(square);
-		m_ActiveScene->Reg().emplace<SpriteRendererComponent>(square, glm::vec4{ 0.0f, 1.0f, 0.0f, 1.0f });
+		square.AddComponent<SpriteRendererComponent>(glm::vec4{ 0.0f, 1.0f, 0.0f, 1.0f });
 
 		m_SquareEntity = square;
 	}
@@ -142,8 +142,16 @@ namespace GameEngine {
 		ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
 		ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
 
-		auto& squareColor = m_ActiveScene->Reg().get<SpriteRendererComponent>(m_SquareEntity).Color;
-		ImGui::ColorEdit4("Square Color", glm::value_ptr(squareColor));
+		if (m_SquareEntity)
+		{
+			ImGui::Separator();
+			auto& tag = m_SquareEntity.GetComponent<TagComponent>().Tag;
+			ImGui::Text("%s", tag.c_str());
+
+			auto& sqaureColor = m_SquareEntity.GetComponent<SpriteRendererComponent>().Color;
+			ImGui::ColorEdit4("Square Color", glm::value_ptr(sqaureColor));
+			ImGui::Separator();
+		}
 
 		ImGui::End();
 
--- GameEngine/src/GameEngine.h
+++ GameEngine/src/GameEngine.h
@@ -18,6 +18,7 @@
 #include "GameEngine/ImGui/ImGuiLayer.h"
 
 #include "GameEngine/Scene/Scene.h"
+#include "GameEngine/Scene/Entity.h"
 #include "GameEngine/Scene/Components.h"
 
 // ---Renderer------------------------
