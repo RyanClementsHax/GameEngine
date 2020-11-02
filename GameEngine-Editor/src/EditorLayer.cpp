0d5e3359c6bc83bf9cc86c304c75f6814aa9dc02
Ryan Clements <ryanclementshax@gmail.com>
2020-11-01 20:29:44 -0500

    Camera Component UI

--- GameEngine-Editor/src/EditorLayer.cpp
+++ GameEngine-Editor/src/EditorLayer.cpp
@@ -25,8 +25,12 @@ namespace GameEngine {
 		m_ActiveScene = CreateRef<Scene>();
 
 		// Entity
-		auto square = m_ActiveScene->CreateEntity();
+		auto square = m_ActiveScene->CreateEntity("Green Square");
 		square.AddComponent<SpriteRendererComponent>(glm::vec4{ 0.0f, 1.0f, 0.0f, 1.0f });
+
+		auto redSquare = m_ActiveScene->CreateEntity("Red Square");
+		redSquare.AddComponent<SpriteRendererComponent>(glm::vec4{ 1.0f, 0.0f, 0.0f, 1.0f });
+
 
 		m_SquareEntity = square;
 
