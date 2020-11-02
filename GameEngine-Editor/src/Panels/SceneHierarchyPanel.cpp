72be6f5ef5df1f51965661cd32db4063298320ea
Ryan Clements <ryanclementshax@gmail.com>
2020-11-01 20:12:58 -0500

    Properties Panel

--- GameEngine-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ GameEngine-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -1,5 +1,7 @@
 #include "SceneHierarchyPanel.h"
 
+#include <glm/gtc/type_ptr.hpp>
+
 #include <imgui/imgui.h>
 
 #include "GameEngine/Scene/Components.h"
@@ -27,6 +29,12 @@ namespace GameEngine {
 		});
 
 		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+			DrawComponents(m_SelectionContext);
+
+		ImGui::End();
 	}
 
 	void SceneHierarchyPanel::DrawEntityNode(Entity entity)
@@ -47,6 +55,33 @@ namespace GameEngine {
 			if (opened)
 				ImGui::TreePop();
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
 		}
 	}
 
