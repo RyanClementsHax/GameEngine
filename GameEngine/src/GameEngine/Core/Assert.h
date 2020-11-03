082fa9afecbaad103da810fe3baa07b3b9ad1cad
Ryan Clements <ryanclementshax@gmail.com>
2020-11-02 21:37:19 -0500

    Saving and Loading Scenes

--- GameEngine/src/GameEngine/Core/Assert.h
+++ GameEngine/src/GameEngine/Core/Assert.h
@@ -10,7 +10,7 @@
 	// provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
 	#define GE_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { GE##type##ERROR(msg, __VA_ARGS__); GE_DEBUGBREAK(); } }
 	#define GE_INTERNAL_ASSERT_WITH_MSG(type, check, ...) GE_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-	#define GE_INTERNAL_ASSERT_NO_MSG(type, check) HZ_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", HZ_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
+	#define GE_INTERNAL_ASSERT_NO_MSG(type, check) GE_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", GE_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
 	#define GE_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
 	#define GE_INTERNAL_ASSERT_GET_MACRO(...) GE_EXPAND_MACRO( GE_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, GE_INTERNAL_ASSERT_WITH_MSG, GE_INTERNAL_ASSERT_NO_MSG) )
