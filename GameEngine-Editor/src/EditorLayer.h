e3d7036be81c11f781c44e963c5bcd9b718c03cd
Ryan Clements <ryanclementshax@gmail.com>
2020-11-02 21:47:33 -0500

    Open/Save File Dialogs

--- GameEngine-Editor/src/EditorLayer.h
+++ GameEngine-Editor/src/EditorLayer.h
@@ -18,6 +18,12 @@ namespace GameEngine {
 		virtual void OnImGuiRender() override;
 		void OnEvent(Event& e) override;
 	private:
+		bool OnKeyPressed(KeyPressedEvent& e);
+
+		void NewScene();
+		void OpenScene();
+		void SaveSceneAs();
+	private:
 		GameEngine::OrthographicCameraController m_CameraController;
 
 		// Temp
