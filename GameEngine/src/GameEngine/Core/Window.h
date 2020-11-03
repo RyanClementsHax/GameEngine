1c211150148a2ccf3c7c0f4c0195eb8884fa724e
Ryan Clements <ryanclementshax@gmail.com>
2020-11-02 21:16:30 -0500

    Making the Hazelnut Editor Look Good

--- GameEngine/src/GameEngine/Core/Window.h
+++ GameEngine/src/GameEngine/Core/Window.h
@@ -14,8 +14,8 @@ namespace GameEngine {
 		uint32_t Height;
 
 		WindowProps(const std::string& title = "Game Engine",
-					 uint32_t width = 1280,
-					 uint32_t height = 720)
+					 uint32_t width = 1600,
+					 uint32_t height = 900)
 			:Title(title), Width(width), Height(height)
 		{ }
 	};
